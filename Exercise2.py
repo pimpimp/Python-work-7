c852943cd047521394de389c057d030957b7de5f
pimpimp <138181820+pimpimp@users.noreply.github.com>
2024-01-09 07:27:09 +0700

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,17 @@
+#2
+score = int(input())
+grade = ""
+if score >= 80:
+    grade = "A"
+elif 70 <= score <= 79:
+    grade = "B"
+elif 60 <= score <= 69:
+    grade = "C"
+elif 50 <= score <= 59:
+    grade = "D"
+else:
+    grade = "F"
+
+print(grade)
+  
+  
